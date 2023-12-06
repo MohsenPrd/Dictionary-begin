bc03c0e2b66abb8a1199aa1bce5e99920b57728b
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2023-12-06 19:08:27 +0330

    Create Dictionary-begin.py

---
+++ Dictionary-begin.py
@@ -0,0 +1,32 @@
+## Mohsen Pourdehghan
+
+shoppingStore = {'meat' : 100,
+                'bread' : 5,
+                'juice' : 20,
+                'cheese' : 30,
+                'butter' : 40,
+                'snack' : 25,
+                'nuts' : 150,
+                'water' : 5,
+                'chocolate' : 20,
+                'milk' : 30}
+                  
+
+organization = { 'name' : 'World Health Organization',
+                 'found' : 1948,
+                 'country' : 'Switzerland',
+                 'budget' : 7.96
+                 #billion
+                 }
+
+
+class_students = { 'name' : ['mohsen' , 'amir', 'hossein', 'saeed'],
+             'age' : [20, 21, 23, 19],
+             'avg' : [18, 16, 18.5, 17],
+             'height' : [179, 181, 175, 188]}
+
+print(f' Shopping store =\n{shoppingStore}')
+
+print('\n',f'Organization =\n{organization}')
+
+print('\n',f'Class students =\n{class_students}')
